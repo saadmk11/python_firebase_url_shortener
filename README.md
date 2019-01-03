77096ea929fa7ef050e2f5b461ca86727457c264
saadmk11 <saad.mk11@hotmail.com>
2019-01-04 03:20:22 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Firebase Dynamic Links URL Shortener 
+
+This is a Python Client for Firebase Dynamic Links to Create Short URLs.
+
+## Requirements
+
+Be sure you have the following installed on your development machine:
+
++ Python >= 3.6.3
++ requests
+
+
+## Usage
+
+```bash
+from url_shortener.url_shortener import UrlShortener
+
+url_shortener = UrlShortener('your_api_key', 'domain_name')
+url_shortener.get_short_link('https://www.example.com/')
+```
+
+## Note
+Here Domain Name Refers to The Subdomain you set for your Dynamic Links.
---
+++ url_shortener/url_shortener.py
@@ -0,0 +1,28 @@
+import requests
+
+
+class UrlShortener:
+    def __init__(self, api_key, domain):
+        firebase_api_url = (
+            'https://firebasedynamiclinks.googleapis.com/v1/shortLinks?key={}')
+        self.api_url = firebase_api_url.format(api_key)
+        self.domain = domain
+
+    def get_short_link(self, long_url):
+        url = "https://{}.page.link/?link={}&apn=com.{}.android&ibi=com.{}.ios"
+        long_dynamic_link = url.format(
+            self.domain, long_url, self.domain, self.domain)
+        print(long_dynamic_link)
+
+        payload = {
+            "longDynamicLink": long_dynamic_link
+        }
+        response = requests.post(self.api_url, json=payload)
+
+        json_data = response.json()
+        print(json_data)
+
+        if response.status_code != 200:
+            return json_data
+
+        return json_data.get('shortLink')
